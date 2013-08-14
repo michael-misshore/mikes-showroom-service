a398d90bff56455ede2f60139a243d4fc59a8b2b
Michael Misshore <michael@mudbugmedia.com>
2013-08-13 19:47:07 -0500

    Config.ru, adding the Sinatra gem, and requiring the new relic gem

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'http://rubygems.org'
 
 gem 'rack'
 gem 'newrelic_rpm'
+gem 'sinatra'
--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 #!/usr/bin/env ruby
 require 'sinatra'
 
+configure :production do
+  require 'newrelic_rpm'
+end
+
+
 get '/' do
   erb :index
 end
