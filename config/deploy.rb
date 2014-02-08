ea466eda51bebdabb620b42d8278c8d655165fdf
Michael Misshore <michael@mudbugmedia.com>
2014-02-08 10:38:14 -0600

    Unicorn restart cap task

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,8 +6,16 @@ require 'capistrano3/unicorn'
 set :application, 'mikesshowroomservice.com'
 set :repo_url, 'git@github.com:michael-misshore/mikes-showroom-service.git'
 set :linked_files, %w{.env}
+set :unicorn_pid, '/home/unicorn/pids/unicorn.pid'
+set :unicorn_config_path, '/home/unicorn/unicorn.conf'
+set :unicorn_roles, %w{app}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-after 'deploy:restart', 'unicorn:reload'
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:reload'
+  end
+end
