ca3da7741ec50a81ae6ca2d27d70489a057e7362
Michael Misshore <michael@mudbugmedia.com>
2014-02-13 12:57:54 -0600

    Move log directory to a shared directory via capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,12 +6,10 @@ require 'capistrano3/unicorn'
 set :application, 'mikesshowroomservice.com'
 set :repo_url, 'git@github.com:michael-misshore/mikes-showroom-service.git'
 set :linked_files, %w{.env}
+set :linked_dirs, %w{log}
 set :unicorn_pid, '/home/unicorn/pids/unicorn.pid'
 set :unicorn_config_path, '/home/unicorn/unicorn.conf'
 set :unicorn_roles, %w{app}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
